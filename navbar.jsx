388ae044ad29b36b238eafe3522726ccf0ec9443
Madhumitha <167957749+madhumithaponnusamy@users.noreply.github.com>
2026-01-08 08:39:36 +0530

    Add files via upload

---
+++ navbar.jsx
@@ -0,0 +1,34 @@
+import { Link, Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./navbar.css";
+
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+   
+    localStorage.removeItem("user_token");
+    localStorage.removeItem("user_role");
+    navigate("/");
+  };
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/user/dashboard">Dashboard</Link>
+        <Link to="/myreport">My Reports</Link>
+
+        {localStorage.getItem("user_token") && (
+          <button  onClick={handleLogout}>
+            Logout
+          </button>
+        )}
+
+      </nav>
+
+      <Outlet />
+    </>
+  );
+}
+
+
